5babf3c69babd86a0ae06b5cfd1ed9a77d07b440
Tefymahery <mihossiraman@gmail.com>
2024-12-10 18:32:29 +0300

    Appbar with mui + Gestion Hook

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,29 @@
-export default function About() {
-    return <h1>À propos de cette application</h1>;
-  }
-  
+import { Button, Snackbar } from '@mui/material';
+import { useState, useEffect } from 'react';
+
+export default function App() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    // Par exemple, afficher le Snackbar après 3 secondes
+    const timer = setTimeout(() => setOpen(true), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Snackbar
+        open={open}
+        message="Bienvenue sur MUI!"
+        onClose={handleClose}
+      />
+      <Button variant="contained" onClick={() => setOpen(true)}>
+        Afficher Notification
+      </Button>
+    </div>
+  );
+}
